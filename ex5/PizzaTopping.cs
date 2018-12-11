ff52091b981a5cec9c69c31c14cf087c911495d1
MPAVE <45466369+MPAVE@users.noreply.github.com>
2018-12-11 10:07:01 +0200

    Add files via upload

---
+++ ex5/PizzaTopping.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex5.pizza
+{
+    class PizzaTopping
+    {
+        public string Topping { get; set; }
+        public double Cost { get; set; }
+        public void Print()
+        {
+            Console.WriteLine("Toping: " + Topping);
+            Console.WriteLine("Pretul:" + Cost);
+        }
+    }
+}
---
+++ ex5/pizza.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex5.pizza
+{
+    class pizza
+    {
+
+        public pizza(string name, PizzaBase pizzaBase, List<PizzaTopping> listatopinguri)
+        {
+            Name = name;
+            Base = pizzaBase;
+            Listatopinguri = listatopinguri;
+        }
+
+        public string Name { get; set; }
+
+        public PizzaBase Base { get; set; }
+
+        public List<PizzaTopping> Listatopinguri
+        { get; set; }
+
+        public double Cost { get; set; }
+
+        public void AddTopping(PizzaTopping pizzaTopping)
+        {
+            Listatopinguri.Add(pizzaTopping);
+        }
+        public string Print()
+        {
+            return $"Pizza {Name}";
+
+        }
+
+        public string CalculateTotalCost()
+        {
+
+        }
+    }
+}
