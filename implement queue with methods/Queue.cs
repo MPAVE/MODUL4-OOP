e5bc201244f7b273e8d2ca7c0eb7776b805e9fff
MPAVE <45466369+MPAVE@users.noreply.github.com>
2018-12-14 15:11:32 +0200

    Add files via upload

---
+++ implement queue with methods/Queue.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace implement_queue_with_methods
+{
+    class Queue
+    {
+   
+       public List<int> q = new List<int>();
+
+        public void Enqueue(int i)
+            {
+                q.Add(i);
+            }
+            public void Dequeue()
+            {
+                q.RemoveAt(0);
+            }
+         public void DisplayQueue()
+        {
+            foreach (int i in q)
+             Console.WriteLine(i + " "); 
+        }
+
+    }
+}
---
+++ implement queue with methods/implement queue with methods.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace implement_queue_with_methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue q = new Queue();
+
+            q.Enqueue(1);
+            q.Enqueue(2);
+            q.Enqueue(3);
+            q.Enqueue(4);
+
+            Console.Write("queue: ");
+            q.DisplayQueue();
+            q.Dequeue();
+            Console.WriteLine("dequeue");
+            q.DisplayQueue();
+            Console.ReadLine();
+        }
+       
+    }
+}
