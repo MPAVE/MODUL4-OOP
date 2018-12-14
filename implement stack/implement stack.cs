2825a7cf38aead0bfab8d1d6050ccbbc56fc07d5
MPAVE <45466369+MPAVE@users.noreply.github.com>
2018-12-14 15:12:47 +0200

    Add files via upload

---
+++ implement stack/implement stack.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace implement_stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            string a = Convert.ToString(stack.Pop());
+            Console.WriteLine("ultimul element din coada este:" + a);
+            //string b = Convert.ToString(stack.Peek());
+            // Console.WriteLine(b);
+            foreach (var item in stack)
+
+            {
+                Console.WriteLine("\t" + item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
