8258385869ff55b4474070fdb9d3c4776e0e2f1d
MPAVE <45466369+MPAVE@users.noreply.github.com>
2018-12-14 15:13:59 +0200

    Add files via upload

---
+++ implement stack with methods/Stack.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace implement_stack_with_methods
+{
+    class Stack
+    {
+        public List<int> stack = new List<int>();
+
+        public void Push(int i)
+        {
+            stack.Add(i);
+        }
+        public void Pop()
+        {
+            stack.RemoveAt(stack.Count - 1);
+        }
+        public void DisplayStack()
+        {
+            foreach (var item in stack)
+
+            {
+                Console.WriteLine("\t" + item);
+            }
+        }
+    }
+}
---
+++ implement stack with methods/implement stack with methods.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace implement_stack_with_methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            Console.WriteLine("elementele din coada sunt:");
+            stack.DisplayStack();
+
+            stack.Pop();
+            Console.WriteLine("coada dupa iesirea unui element:");
+            stack.DisplayStack();
+            Console.ReadLine();
+        }
+    }
+}
